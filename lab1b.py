4601e4ee6295585ad4442acd9bd1612da0b0ba45
amohamed176 <amohamed176@myseneca.ca>
2025-01-22 22:45:01 -0500

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Description: This program demonstrates string operations in Python.'''
+
+# Assigning the value "Isaac" to the name variable
+name = 'Isaac'
+
+# Printing the required statement
+print('How old are you ' + name + '?')
