113471ca6d75ab47c54b83f51244756792159661
amohamed176 <amohamed176@myseneca.ca>
2025-01-22 23:02:02 -0500

    Fixed output issue in lab1d.py

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: This program demonstrates mathematical operations in Python.'''
+
+# Define integer variables
+x = 10
+y = 2
+z = 5
+
+# Print the required output only
+print(f"{x} + {y} * {z} = {x + y * z}")
