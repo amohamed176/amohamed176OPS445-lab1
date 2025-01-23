36fee66f263ccbe669cfd39187f095908423eb65
amohamed176 <amohamed176@myseneca.ca>
2025-01-22 22:57:55 -0500

    Fixed output issue in lab1c.py

---
+++ lab1c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Description: This program demonstrates integer operations in Python.'''
+
+# Assigning values to variables
+name = 'Isaac'
+age = 72
+
+# Printing the required output
+print(name + ' is ' + str(age) + ' years old!')
+
